7a2e6ddb1bd4da306ae0f91a0543b899fa1add5d
cakejune <tyq444@gmail.com>
2022-10-17 14:29:03 -0400

    commit tree 8e0a9473bc3f590afad74a5cf2a9173b4d744e0a

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
+  // To pass the tests, don't forget to return your fetch!\
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => renderBooks(json)); 
+  //.then((json) => console.log(`Entry number ${int} is the book: "${json[int].name}"`)) 
+
+}  
 
 function renderBooks(books) {
   const main = document.querySelector('main');
